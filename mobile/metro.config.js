bb63a6b6f502ba880153b2c778fecbd76b9eba12
Keith Codner <codnerkj@gmail.com>
2026-02-20 23:25:38 -0500

    update

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,8 +1,6 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const { withNativeWind } = require("nativewind/metro");
- 
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  /* your config */
-});
- 
-module.exports = withNativeWind(config, { input: "./global.css" });
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './global.css' });
