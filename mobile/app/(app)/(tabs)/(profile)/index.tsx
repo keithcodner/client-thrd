7fea5dfd221db8354336d9804c681172cb79cd4f
Keith Codner <codnerkj@gmail.com>
2026-03-05 23:59:21 -0500

    update

--- mobile/app/(app)/(tabs)/(profile)/index.tsx
+++ mobile/app/(app)/(tabs)/(profile)/index.tsx
@@ -1,9 +1,40 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Button, Text, ScrollView, Platform, Alert, TouchableOpacity } from "react-native";
 import { useThemeColours } from "@/hooks/useThemeColours";
+import { useSession } from '@/context/AuthContext';
 
 const Profile = () => {
   const colours = useThemeColours();
+
+  const { user, signOut } = useSession();
+  const colors = useThemeColours();
+
+  const handleLogout = () => {
+    if (Platform.OS === 'web') {
+      // For web browsers
+      if (window.confirm('Are you sure you want to logout?')) {
+        signOut?.();
+      }
+    } else {
+      // Logic for other platforms goes here
+      // For mobile devices
+      Alert.alert(
+        'Logout',
+        'Are you sure you want to logout?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'Logout',
+            style: 'destructive',
+            onPress: () => signOut?.(),
+          }
+        ],
+      );
+    }
+  };
 
   return (
     <View className="flex-1 bg-white dark:bg-gray-900">
@@ -15,6 +46,13 @@ const Profile = () => {
           <Text className="text-gray-600 dark:text-gray-400 mt-2">
             Welcome to THRD Profile Section
           </Text>
+
+          <TouchableOpacity
+            onPress={handleLogout}
+            className="rounded-2xl shadow-lg"
+            >
+                <Text className="text-lg font-bold">Logout</Text>
+        </TouchableOpacity>
         </View>
       </ScrollView>
     </View>
